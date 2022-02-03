fd01a36aa26e440b6fd26ae0016ee811ef9efc51
Lumijek <lumijekpr@gmail.com>
2022-02-02 19:22:57 -0800

    New implementation

--- merge_sort.py
+++ merge_sort.py
@@ -1,4 +1,39 @@
+from random import shuffle
+import time
 from array import array
+
+x = list(range(10000000))
+shuffle(x)
+print(len(x))
+
+#VERSION 1
+def mergesort(n):
+
+	if len(n) <= 1:
+		return n
+
+	length = len(n) // 2
+	A = mergesort(n[:length])
+	B = mergesort(n[length:])
+	iterA = 0
+	iterB = 0
+	C = []
+	for i in range(len(n)):
+		if A[iterA] < B[iterB]:
+			C.append(A[iterA])
+			iterA += 1
+		elif B[iterB] < A[iterA]:
+			C.append(B[iterB])
+			iterB += 1
+		if iterA == len(A):
+			C += B[iterB:]
+			break
+		if iterB == len(B):
+			C += A[iterA:]
+			break
+	return C
+
+#VERSION 2
 def merge_sort(n):
 
 	if len(n) <= 1:
@@ -33,4 +68,3 @@ def merge_sort(n):
 				iterC += 1
 			break
 	return C
-
